9296949f18f2bc79f29d2abacf06a2b438acaeba
cristianh <crishernandez08@hotmail.com>
2021-12-27 15:20:25 -0500

    subiendo archivos

---
+++ script.js
@@ -0,0 +1,268 @@
+
+let video;
+
+let elementoActualSound = null;
+let audioHimno = null;
+let audiopage = null;
+let myAnimation
+
+function init() {
+    audioHimno = new Sonidos('Himno_Colombia');
+    audiopage = new Sonidos('HojaFlip');
+    document.body.addEventListener('keyup', presentacionteclado, false)
+
+    setTimeout(() => {
+        cargaPageFlip()
+        document.querySelector('.spinner').classList.add('hidden')
+        document.querySelector('.flipbook-viewport').classList.remove('hidden')
+        /* let el = document.querySelector('#Bandera02');
+         myAnimation= new LazyLinePainter(el, { "ease": "easeInOutQuad", "strokeWidth": 1.1, "strokeOpacity": 0.5, "strokeColor": "#000000", "strokeCap": "square" }); */
+    }, 999);
+    
+
+    // Load the HTML4 version if there's not CSS transform
+
+    /* yepnope({
+        test: Modernizr.csstransforms,
+        yep: ['../../lib/turn.js'],
+        nope: ['../../lib/turn.html4.min.js'],
+        both: ['css/basic.css'],
+        complete: cargaPageFlip
+    }); */
+
+    
+
+}
+
+
+function cargaPageFlip() {
+
+    let flippage = document.querySelector('.flipbook')
+
+    // Create the flipbook
+
+    $(flippage).turn({
+        // Width
+
+        width: 922,
+
+        // Height
+
+        height: 600,
+
+        // Elevation
+
+        elevation: 50,
+
+        // Enable gradients
+
+        gradients: true,
+
+        // Auto center this flipbook
+
+        autoCenter: true
+
+    });
+
+    /* window.addEventListener('resize', (e) => {
+        flippage.style.width = '';
+        flippage.style.height = '';
+        $(flippage).turn('size', flippage.clientWidth, flippage.clientHeight);
+        $(flippage).turn({
+            autoCenter: true
+        });
+    }); */
+
+    
+    
+
+    let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+     if (!isMobile) {
+        /*  $('.flipbook').turn('size', 400, 535); */
+         /* $('.flipbook').turn('resize'); */
+     }else{
+         /* $(".flipbook").turn({width:640, height:623}); */
+         $(flippage).turn('display', 'single');
+         $(flippage).turn('size',  400, 635);
+         $(flippage).turn('resize')
+        /* $(flippage).turn({
+            autoCenter: true
+        }) */;
+     }
+
+     $(flippage).bind('turning', (event, page, view) => {
+       
+        audiopage.playAudio()
+    })
+
+    $(flippage).bind('turned', (event, page, view) => {
+        switch (page) {
+            case 2:
+                break;
+
+            default:
+                break;
+        }
+
+    })
+
+
+}
+
+function presentacionteclado(e) {
+
+
+    if (e.keyCode == 39) {
+        $('.flipbook').turn('next');
+        e.preventDefault();
+        /* siguiente() */
+    }
+
+    if (e.keyCode == 37) {
+        $('.flipbook').turn('previous');
+        e.preventDefault();
+
+    }
+}
+
+function playSonido(id) {
+
+    audioHimno.playAudio(verificarFinAudio)
+    elementoActualSound = `#${id}`
+    document.querySelector(`#${id}`).removeEventListener('click', playSonido, false)
+    document.querySelector(`#${id}`).addEventListener('click', stopSonido, false)
+    document.querySelector(`#${id}`).classList.remove('botonPlay')
+    document.querySelector(`#${id}`).classList.add('botonPause')
+    //verificarFinAudio(`#${id}`)
+}
+
+function verificarFinAudio() {
+    audioHimno.playAudioFondo()
+    document.querySelector(elementoActualSound).classList.remove('botonPause')
+    document.querySelector(elementoActualSound).classList.add('botonPlay')
+    document.querySelector(elementoActualSound).removeEventListener('click', stopSonido, false)
+    document.querySelector(elementoActualSound).addEventListener('click', playSonido, false)
+}
+
+function stopSonido(e) {
+    audioHimno.stopAudio()
+    document.querySelector(`#${e.target.id}`).removeEventListener('click', stopSonido, false)
+    document.querySelector(`#${e.target.id}`).addEventListener('click', playSonido, false)
+    document.querySelector(`#${e.target.id}`).classList.add('botonPlay')
+    document.querySelector(`#${e.target.id}`).classList.remove('botonPause')
+}
+
+
+function Imprimir(idImagen) {
+
+    let base64StringImagen1 = imagenData(idImagen)
+    let imagenfondoAplicada = imagenData('imagen-fondo')
+
+
+    let ventanaPreview = window.open('', '', 'height=800, width=800');
+    ventanaPreview.document.write('<html>');
+    ventanaPreview.document.write('<head>');
+    ventanaPreview.document.write('<style>');
+    ventanaPreview.document.write(`
+
+
+@media print {
+    #botonImprimir{
+        display:none
+    }
+    textarea{
+        resize: none !important;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+    }
+
+    body{
+        -webkit-print-color-adjust:exact;
+        -moz-print-color-adjust:exact;
+        -ms-print-color-adjust:exact;
+        print-color-adjust:exact;
+    } 
+
+   
+}
+
+@media print and (orientation:landscape)
+{
+    body{
+        -webkit-print-color-adjust:exact;
+        -moz-print-color-adjust:exact;
+        -ms-print-color-adjust:exact;
+        print-color-adjust:exact;
+    }    
+}`);
+    ventanaPreview.document.write('</style>');
+    ventanaPreview.document.write('<script src="https://cdn.tailwindcss.com"></script>');
+    ventanaPreview.document.write('</head>');
+    ventanaPreview.document.write('<body> <h1>Vista previa Impresion<br>');
+    ventanaPreview.document.write(`<div id="print"
+                                    class="w-full h-[70vh]  flex flex-col mx-auto mt-[10%] text-center justify-center items-center content-center border-2 border-gray-100"  style='background-image:${imagenfondoAplicada};background-repeat: no-repeat;background-size: 100% 100%;background-position: center center;'>
+                                    <img class=" w-90 h-90 object-fit "
+                                    src="${base64StringImagen1}" alt="escudo:alt">
+                                </div>`);
+    ventanaPreview.document.write('</body></html>');
+    ventanaPreview.document.close();
+    ventanaPreview.print();
+
+}
+
+
+function imagenData(id) {
+    var c = document.createElement('canvas');
+    var img = document.getElementById(`${id}`);
+    c.height = img.naturalHeight;
+    c.width = img.naturalWidth;
+    var ctx = c.getContext('2d');
+
+    ctx.drawImage(img, 0, 0, c.width, c.height);
+    return c.toDataURL();
+}
+
+// Get the modal
+let modal = document.getElementById("myModal");
+
+
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+/* span.addEventListener('click', ocultarModal); */
+window.addEventListener('click', ocultarModalVentana)
+
+function ocultarModal() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+function ocultarModalVentana(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function MostrarVideo() {
+    modal.style.display = "flex";
+}
+
+function OcultarVideo() {
+    video.currentTime = 0
+    videoMobile.currentTime = 0
+    video.pause()
+    videoMobile.pause()
+    modal.style.display = "none";
+}
+
+function ReproducirVideo(e) {
+    video.play()
+    document.querySelector('#videoOthers').classList.add("disabledbutton")
+    video.addEventListener('ended', function (e) {
+
+        document.querySelector('#videoOthers').classList.remove("disabledbutton")
+
+    })
+}
---
+++ src/sounds.js
@@ -0,0 +1,95 @@
+ class Sonidos{
+
+    audio;
+    audioOvers;
+    audioOversMenu;
+    path_sound = './assets/sounds/'
+    sonido;
+    isFinalizado=false
+    isPlaying=false
+    isPlayingFondo=false
+
+
+    constructor(sonido) {
+    this.sonido=sonido
+    this.cargarAudio()
+    }
+
+    cargarAudio() {
+        this.audio = new Audio(`${this.path_sound}${this.sonido}.mp3`);
+        /* audio.muted = true */    
+    }
+
+    playAudio(callback){
+        /* this.audio.loop = loop */
+        this.audio.muted = false   
+        this.audio.play();
+        this.isPlaying=true
+        this.audio.addEventListener('ended',  () => {
+            this.isFinalizado=true
+            if(callback!=null){
+                callback()
+                this.isPlaying=false
+            }
+           
+        });
+    }
+
+    playAudioFondo(){
+        this.audio.loop = true
+        this.audio.muted = false   
+        this.audio.play();
+        this.isPlayingFondo=true
+    }
+
+    
+    
+   silenciarAudioParlante() {
+        this.audio.muted = !this.audio.muted;
+        if (this.audio.muted) {
+            document.querySelector('#imagen-audio').setAttribute('src', './assets/animaciones/parlanteOff.gif')
+        } else {
+            document.querySelector('#imagen-audio').setAttribute('src', './assets/animaciones/parlanteOn.gif')
+        }
+    }
+    
+  
+    stopAudio() {
+        this.audio.muted = !this.audio.muted;
+        this.audio.currentTime = 0;
+        /* this.audio.currentTime = 0
+        this.audio.muted = true;
+        alert(this.audio.muted) */
+    }
+
+    pauseAudio() {
+        this.audio.pause()
+    }
+
+    getIntance(){
+        return this.audio
+    }
+
+    getFinalizado(){
+        return this.isFinalizado
+    }
+    
+    getPlayingFondo(){
+        return this.isPlayingFondo
+    }
+    
+    getPlaying(){
+        return this.isPlaying
+    }
+    
+
+    changeSound(new_sond) {
+        this.audio.src = `${this.path_sound}${new_sond}.mp3`;
+        this.audio.pause();
+        this.audio.load();
+        this.audio.play();
+      
+    }
+    
+
+}
